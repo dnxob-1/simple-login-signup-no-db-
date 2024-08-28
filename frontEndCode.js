a69f0f52448bde29649c4bf0cbd2b4e9ef3598e2
morgan <ff6.6.ffdnxob.pthd.6@gmail.com>
2024-08-28 16:34:56 -0500

    Add files via upload

---
+++ frontEndCode.js
@@ -0,0 +1,17 @@
+function checkOnVisi() {
+  var input = document.getElementById("accPassword");
+  if (input.type === "password") {
+    input.type = "text";
+  } else {
+    input.type = "password";
+  }
+}
+
+function checkOnVisi2() {
+  var input = document.getElementById("accPassword2");
+  if (input.type === "password") {
+    input.type = "text";
+  } else {
+    input.type = "password";
+  }
+}
